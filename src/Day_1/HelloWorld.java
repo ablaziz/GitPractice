fe4f71ad48cbff3ecf0991f9acfd30daad3caae5
Aziz Niada <niadaaaziz@yahoo.fr>
2021-04-24 18:01:18 -0400

    added one more line of code

--- src/Day_1/HelloWorld.java
+++ src/Day_1/HelloWorld.java
@@ -3,5 +3,6 @@ package Day_1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("Adding GitHub now");
     }
 }

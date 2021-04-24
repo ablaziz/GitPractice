3f62cf4b06ff159080096e62f4e71bbbc42ea6af
Aziz Niada <niadaaaziz@yahoo.fr>
2021-04-24 18:08:29 -0400

    added more work after publishing to GITHUB

--- src/Day_1/HelloWorld.java
+++ src/Day_1/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("Adding GitHub now");
+        System.out.println("Stuffs are on GitHub");
     }
 }
